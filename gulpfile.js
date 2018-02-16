397d5d93b03163df400af9bdeb02088b332288bb
piersh <piersh@hotmail.com>
2018-02-16 11:17:39 -0800

    updated build

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
-var gulp = require('gulp');
-var util = require('gulp-util');
-var ts = require('gulp-typescript');
-var watch = require('gulp-watch');
-//var browserify = require('browserify');
-//var source = require('vinyl-source-stream');
-var less = require('gulp-less');
-var connect = require('gulp-connect');
-//var uglify = require('gulp-uglify');
-var uglifyes = require('uglify-es');
-var composer = require('gulp-uglify/composer');
-var uglify = composer(uglifyes, console);
+const gulp = require('gulp');
+const noop = require('gulp-noop');
+const ts = require('gulp-typescript');
+const watch = require('gulp-watch');
+const less = require('gulp-less');
+const uglifyes = require('uglify-es');
+const composer = require('gulp-uglify/composer');
+const uglify = composer(uglifyes, console);
 
-var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
-var uglifycss = require('gulp-uglifycss');
-var clean = require('gulp-clean');
+const sourcemaps = require('gulp-sourcemaps');
+//const buffer = require('vinyl-buffer');
+const uglifycss = require('gulp-uglifycss');
+const clean = require('gulp-clean');
+const browserSync = require('browser-sync');
+const minimist = require('minimist');
 
-const config = {
-	production: !!util.env.production,
-};
+const config = minimist(process.argv.slice(2), {
+	boolean: [ 'production' ],
+	default: {
+		production: process.env.production
+	}
+});
 
-var tsProject = ts.createProject(
+const tsProject = ts.createProject(
 	'./src/tsconfig.json', {
 		removeComments: config.production,
 		//experimentalAsyncFunctions: !config.production,
@@ -32,86 +33,71 @@ var tsProject = ts.createProject(
 compile typescript
 use ES5 and commonJS module
 */
-gulp.task('typescript', function () {
-
-	return tsProject.src()
+gulp.task('typescript', () =>
+	tsProject.src()
 		.pipe(sourcemaps.init())
 		.pipe(tsProject()).js
 		//.pipe(gulp.dest("dist/js"))
 		//.pipe(buffer())
-		.pipe(config.production ? uglify() : util.noop())
+		.pipe(config.production ? uglify() : noop())
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest("dist"));
-});
+		.pipe(gulp.dest("dist"))
+		.pipe(browserSync.reload({ stream: true, once: true }))
+);
 
-gulp.task('javascript', function () {
+gulp.task('javascript', () =>
 
 	gulp.src('src/**/*.js')
 		.pipe(sourcemaps.init())
-		.pipe(config.production ? uglify() : util.noop())
+		.pipe(config.production ? uglify() : noop())
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist'));
-});
-/*
-Web server to test app
-*/
-gulp.task('webserver', function () {
-	connect.server({
-		livereload: true,
-		root: ['.', 'dist']
-	});
-});
-/*
-Automatic Live Reload
-*/
-gulp.task('livereload', function () {
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.reload({ stream: true, once: true }))
+);
 
-	watch(['dist/styles/*.css', 'dist/*.js', 'dist/*.html'])
-		.pipe(connect.reload());
+gulp.task('node_modules', () => {
+
+	gulp.src('node_modules/monaco-editor/min/vs/**/*').pipe(gulp.dest('dist/vs'));
+	gulp.src('node_modules/document-ready-promise/**/*.js').pipe(gulp.dest('dist/document-ready-promise'));
+
 });
+
 /*
 copy all html files and assets
 */
-gulp.task('html', function () {
+gulp.task('html', () =>
 	gulp.src('src/**/*.html')
-		.pipe(gulp.dest('dist'));
-});
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.reload({ stream: true, once: true }))
+);
 
-gulp.task('assets', function () {
+gulp.task('assets', () =>
 	gulp.src('assets/**/*.*')
-		.pipe(gulp.dest('dist/assets'));
-});
+		.pipe(gulp.dest('dist/assets'))
+		.pipe(browserSync.reload({ stream: true, once: true }))
+);
 /*
 compile less files
 */
-gulp.task('less', function () {
+gulp.task('less', () =>
 	gulp.src('src/styles/style.less')
 		.pipe(less())
 		.pipe(sourcemaps.init())
 		.pipe(config.production ? uglifycss({
 			"maxLineLen": 80,
 			"uglyComments": true
-		}) : util.noop())
+		}) : noop())
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist/styles'));
-});
-/*
-browserify
-now is only for Javascript files
-*/
-gulp.task('browserify', function () {
-	browserify('./dist/js/index.js')
-		.bundle()
-		.pipe(source('index.js'))
-		.pipe(gulp.dest('dist'));
-});
+		.pipe(gulp.dest('dist/styles'))
+		.pipe(browserSync.reload({ stream: true, once: true }))
+);
 
 /*
 Watch typescript and less
 */
-gulp.task('watch', function () {
+gulp.task('watch', ['browser-sync'], function () {
 	gulp.watch('src/styles/*.less', ['less']);
-	gulp.watch(['src/**/*.ts', 'src/**/*.tsx'], ['typescript'/*, 'browserify'*/]);
+	gulp.watch(['src/**/*.ts', 'src/**/*.tsx'], ['typescript']);
 	gulp.watch('src/**/*.js', ['javascript']);
 	gulp.watch('src/**/*.html', ['html']);
 	gulp.watch('assets/**/*.*', ['assets']);
@@ -122,16 +108,23 @@ gulp.task('clean', () =>
 		.pipe(clean())
 );
 
-gulp.on('err', function (e) {
-	console.log(e.err.stack);
-});
+gulp.on('err', (e) =>
+	console.log(e.err.stack)
+);
+
+
+gulp.task('browser-sync', () =>
+	browserSync.init(null, {
+		server: {
+			baseDir: "dist"
+		}
+	})
+);
 
 /*
 default task
 */
 
-gulp.task('default',
-	['less', 'typescript', 'javascript', 'html', 'assets']);
+gulp.task('default', ['less', 'typescript', 'javascript', 'node_modules', 'html', 'assets']);
 
-gulp.task('serve',
-	['default', 'webserver', 'livereload', 'watch']);
+gulp.task('serve', ['default', 'watch']);
